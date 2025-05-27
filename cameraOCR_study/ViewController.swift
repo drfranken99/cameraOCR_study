10580817ea727bd829819512483a84c517bb5f25
drfranken <daranic9@gmail.com>
2025-05-27 17:25:38 +0900

    xcode 프로젝트 생성

---
+++ cameraOCR_study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraOCR_study
+//
+//  Created by drfranken on 5/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
