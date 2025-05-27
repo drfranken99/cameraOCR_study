7c38bc8ba8e31b7f5981cec6eefea272e2060a26
drfranken <daranic9@gmail.com>
2025-05-27 17:38:49 +0900

    ViewController 내용 추가
    Outlet Action 연결
    
    viewDidLoad 추가
    didScan 추가

--- cameraOCR_study/ViewController.swift
+++ cameraOCR_study/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let scanManager = ScanManager()
+    let textRecognizer = TextRecognizer()
+
+    @IBOutlet weak var resultTextView: UITextView!
+    
+    @IBAction func scanDocument(_ sender: Any) {
+        scanManager.presentScanner(from: self)
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        scanManager.delegate = self
+        resultTextView.isEditable = false // 결과화면 수정못하게
     }
 
 
 }
 
+
+
+extension ViewController: ScanManagerDelegate {
+    func didScan(image: UIImage) {
+        textRecognizer.recognizeText(from: image) { [weak self] text in
+            self?.resultTextView.text = text
+        }
+    }
+}
